26966ef55b6e4a25cfb15bb937659fb6a20b2cdc
Chapman Siu <chpmn.siu@gmail.com>
2016-05-29 09:09:07 +1000

    consolidated test files

--- R/tests/testthat/test_jsonliteloading.R
+++
@@ -1,6 +0,0 @@
-
-test_that("jsonlite loads jsonlines properly", {
-  input_dat <- system.file("extdata", "simple.json", package="Dave")
-  sample_dat <- load_jsonlines(input_dat)
-  expect_equal(dim(sample_dat), c(2, 4))
-})
