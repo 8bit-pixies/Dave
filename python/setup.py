c895c1b800d7c97d3546848e0cbd8dde0574c1b0
Chapman Siu <chapman.siu@suncorp.com.au>
2016-05-31 11:55:20 +1000

    added setup info

--- python/setup.py
+++ python/setup.py
@@ -1,1 +1,13 @@
-# version 0.2.1
+from setuptools import setup
+
+VERSION = "0.2.1"
+
+setup(name='dave-ml', 
+      version=VERSION,
+      description='Simple feature store part of the JD toolkit',
+      url='https://github.com/Jules-and-Dave/Dave',
+      author='Chapman Siu', 
+      author_email='chpmn.siu@gmail.com',
+      license='MIT', 
+      packages=['dave'], 
+      zip_safe=False)
