a17e911c92c16d240e9f69c51860931689645e07
Chapman Siu <chpmn.siu@gmail.com>
2016-06-01 09:21:11 +1000

    add exception test for schema

--- python/Dave/tests/factsets_tests.py
+++ python/Dave/tests/factsets_tests.py
@@ -1,11 +1,11 @@
 from nose.tools import assert_equal
-from Dave.factset import load_factsets, flatten_dataframe, export_factsets, load_jsonlines
+from Dave.factset import load_factsets, flatten_dataframe, export_factsets, load_jsonlines, check_schema
 
 
-def test_simplejson():    
+def test_simplejson():
     factsets = load_factsets("Dave/tests/data/simple.json", {'entity': 'id', 'datetime':'as_at'})
     assert_equal(set(factsets.columns), set(['as_at', 'zipcode', 'id', 'gender']))
-    
+
 def test_flatten():
     import pandas as pd
     df = pd.DataFrame({
@@ -13,9 +13,12 @@ def test_flatten():
         'datetime': [1,1],
         'info1': [None, 1],
         'info2': [2, None]
-    })    
+    })
     assert_equal(flatten_dataframe(df, export='dict'), [{'info1': 1.0, 'datetime': 1.0, 'info2': 2.0, 'entity': 1.0}])
 
 def test_export():
     import json
     assert_equal(export_factsets([{'a':1}]), '{"a": 1}')
+
+def test_schemaexception():
+    assert_raises(Exception, check_schema, {'a':1})
