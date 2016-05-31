32019aecc34a65cc93d4407923f2acc49c69f1a6
Chapman Siu <chpmn.siu@gmail.com>
2016-06-01 09:27:36 +1000

    corrected tests for loadjsonlines

--- python/Dave/tests/factsets_tests.py
+++ python/Dave/tests/factsets_tests.py
@@ -1,10 +1,15 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_raises
 from Dave.factset import load_factsets, flatten_dataframe, export_factsets, load_jsonlines, check_schema
 
 
 def test_simplejson():
     factsets = load_factsets("Dave/tests/data/simple.json", {'entity': 'id', 'datetime':'as_at'})
     assert_equal(set(factsets.columns), set(['as_at', 'zipcode', 'id', 'gender']))
+
+def test_loadjson():
+    factsets_stream = load_jsonlines("Dave/tests/data/simple.json", stream=True)
+    factsets_nostream = load_jsonlines("Dave/tests/data/simple.json", stream=False)
+    assert_equal(set(factsets_nostream.columns), set(factsets_stream.columns))
 
 def test_flatten():
     import pandas as pd
